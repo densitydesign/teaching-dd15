39275a5d5f3ed4593372107054a033b1e6db4d6d
andrea <benedetti.93@gmail.com>
2019-09-05 10:44:41 +0200

    link to course manifesto

--- code/booklet.js
+++ code/booklet.js
@@ -8,8 +8,8 @@ const Syllabus = ({ _body, _relativeURL, _ID }) => (
 	<Drawer text="Contents"/>
 	<aside id="syllabus--aside">
 	<div id="table--of--content">
-
 	</div>
+	<a className="external--link" href="https://www11.ceda.polimi.it/schedaincarico/schedaincarico/controller/scheda_pubblica/SchedaPublic.do?&evn_default=evento&c_classe=711242&polij_device_category=DESKTOP&__pj0=0&__pj1=1d66ef3d240ce3c2c72179cd04e6221a" target="_blank"><i className="fas fa-external-link-square-alt"></i> Course Manifesto</a>
 	</aside>
 	<main id="right--align">
 	{ _body }
